f1300afb8b0a071690dda04fe3e867d2003dc9ba
Kevin Le <kevinphanle11@gmail.com>
2020-06-21 11:02:11 -0700

    styling nav

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -8,21 +8,24 @@ const Navbar = () => {
 
   return (
     <div className="nav" >
-      <div className="title">
-        Google Keep Clone
-      </div>
+      <div className="navcontent">
 
-      <div className="rightnav">
-        <a href="https://github.com/kevinphanle" target="_blank" rel="noopener noreferrer">
-          <img src={github} alt="" />
-        </a>
-        <a href="https://github.com/kevinphanle" target="_blank" rel="noopener noreferrer">
-          <img src={facebook} alt="" />
-        </a>
-        <a href="https://github.com/kevinphanle" target="_blank" rel="noopener noreferrer">
-          <img src={linkedin} alt="" />
-        </a>
+        <div className="title">
+          Google Keep Clone
+        </div>
 
+        <div className="rightnav">
+          <a href="https://github.com/kevinphanle" target="_blank" rel="noopener noreferrer">
+            <img src={github} alt="" />
+          </a>
+          <a href="https://github.com/kevinphanle" target="_blank" rel="noopener noreferrer">
+            <img src={facebook} alt="" />
+          </a>
+          <a href="https://github.com/kevinphanle" target="_blank" rel="noopener noreferrer">
+            <img src={linkedin} alt="" />
+          </a>
+
+        </div>
       </div>
     </div>
   )
