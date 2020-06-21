e993b012bc0a9f77d3b6e126f643b0d03337568f
Kevin Le <kevinphanle11@gmail.com>
2020-06-21 10:55:06 -0700

    started navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
+import facebook from '../images/facebook.svg';
+import github from '../images/github.svg';
+import linkedin from '../images/linkedin.svg';
+
 const Navbar = () => {
 
   return (
     <div className="nav" >
       <div className="title">
         Google Keep Clone
+      </div>
+
+      <div className="rightnav">
+        <a href="https://github.com/kevinphanle" target="_blank" rel="noopener noreferrer">
+          <img src={github} alt="" />
+        </a>
+        <a href="https://github.com/kevinphanle" target="_blank" rel="noopener noreferrer">
+          <img src={facebook} alt="" />
+        </a>
+        <a href="https://github.com/kevinphanle" target="_blank" rel="noopener noreferrer">
+          <img src={linkedin} alt="" />
+        </a>
+
       </div>
     </div>
   )
